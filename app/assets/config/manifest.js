e3fb090f3a932da153b5ffe0c93d55042a445746
humberto <humberto-_-lzc@hotmail.com>
2020-11-29 17:57:41 -0600

    efe

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
-//= link app.css
-//= link app-modern.css
-//= link icons.css
-//= link app.js
-//= link vendor.js
