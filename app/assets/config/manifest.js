da2cb48887ad629fd394b9633cb7c2e3f3f8bab3
humberto <humberto-_-lzc@hotmail.com>
2020-11-29 17:35:53 -0600

    ddd

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
+//= link app.css
+//= link app-modern.css
+//= link icons.css
+//= link app.js
+//= link vendor.js
