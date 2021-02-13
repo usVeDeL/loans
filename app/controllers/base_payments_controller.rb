7866600817f3ad9adb53544dd83db977082412cf
humberto <humberto-_-lzc@hotmail.com>
2021-02-13 14:33:25 -0600

    edede

--- app/controllers/base_payments_controller.rb
+++ app/controllers/base_payments_controller.rb
@@ -96,7 +96,7 @@ class BasePaymentsController < ApplicationController
       payment_date = (loan.start_date + (n-1).week)
 
       week_payment = loan.weekly_amount
-      week_payment = payment&.loan_movement&.amount.to_f if payment_date < 1.week.ago
+      week_payment = payment&.loan_movement&.amount.to_f if n > 1
 
       percent_capital = CAPITAL_PAYMENT_TABLE[n.to_s.to_sym]
       payment_capital = week_payment*(percent_capital.to_f/100.0)
