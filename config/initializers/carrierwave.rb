4551c962ae76cd525b486d84c404659626802691
humberto <humberto-_-lzc@hotmail.com>
2021-02-19 19:50:14 -0600

    ded

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required unless using use_iam_profile
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required unless using use_iam_profile
+    aws_access_key_id:    'AKIAT6BJJ77RF75QJFOZ',                        # required unless using use_iam_profile
+    aws_secret_access_key: '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',                        # required unless using use_iam_profile
     region:                'us-east-2'                  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'vedel',                                     # required
-  config.fog_public     = true                                                 # optional, defaults to true
+  config.fog_directory  = 'vedel'
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
