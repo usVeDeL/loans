e4e8d8c78aebcf720212aaa3acaa568a630b228a
humberto <humberto-_-lzc@hotmail.com>
2021-02-19 18:36:42 -0600

    update carriewrwave

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required unless using use_iam_profile
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required unless using use_iam_profile
+    region:                'us-east-2'                  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = ENV['BUCKET_NAME'],                                     # required
+  config.fog_public     = true                                                 # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+end
