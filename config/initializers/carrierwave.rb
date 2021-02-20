c4bcdbb2c3393ce20c5278645bba6bbfacb3b7af
humberto <humberto-_-lzc@hotmail.com>
2021-02-19 19:15:20 -0600

    dwdw

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required unless using use_iam_profile
     region:                'us-east-2'                  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = ENV['BUCKET_NAME'],                                     # required
+  config.fog_directory  = 'vedel',                                     # required
   config.fog_public     = true                                                 # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
